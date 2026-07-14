d443f4ce9232d600ac1aff348aa1437208124a2a
jeet <jeetproxy@gmail.com>
2026-07-14 19:58:39 +0530

    initial commit

---
+++ src/components/Herosection.tsx
@@ -0,0 +1,38 @@
+
+
+const Herosection = () => {
+
+
+  return (
+    <div className="w-screen h-screen flex items-center justify-center bg-[#F5F4EB]">
+      <div className="h-[80vh] w-[80vw] flex flex-col">
+        <div className="text-[12vw] font-[rf] text-left -mb-20 text-[#eea813] flex">
+          <div>F</div>
+          <div>R</div>
+          <div>O</div>
+          <div>N</div>
+          <div>T</div>
+          <div>E</div>
+          <div>N</div>
+          <div>D</div>
+        </div>
+        <div className="text-[12vw] font-[rf] text-right -my-10 text-[#eea813] flex justify-end">
+          <div>D</div>
+          <div>E</div>
+          <div>V</div>
+          <div>E</div>
+          <div>L</div>
+          <div>O</div>
+          <div>P</div>
+          <div>E</div>
+          <div>R</div>
+        </div>
+        <div className="text-[4vw] font-[md] -mt-5 text-left">
+          Design and Create
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Herosection
